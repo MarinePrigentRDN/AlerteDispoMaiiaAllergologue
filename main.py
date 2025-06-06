56e3bbbeeece4ca77bd803e25f4f2b68a7db005f
MarinePrigentRDN <marine.prigent.rdn@gmail.com>
2025-06-06 13:52:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
+import os
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 import time
 import requests
 
-# Test workflow
-
-# Configuration Telegram
-TELEGRAM_TOKEN = '7274166974:AAGnCgtxUIqqTFNlsSPSjmqp41NBX89gyNk'
-CHAT_ID = '7800997599'
+# Récupération sécurisée des variables
+TELEGRAM_TOKEN = os.environ['TELEGRAM_TOKEN']
+CHAT_ID = os.environ['CHAT_ID']
 
 def envoyer_alerte(message):
     url = f'https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage'
@@ -27,12 +26,10 @@ try:
     driver.get(url)
     time.sleep(5)
 
-    # Clique sur le bouton pour choisir un motif
     motif_btn = driver.find_element(By.XPATH, '//button[contains(text(), "Motif de consultation")]')
     motif_btn.click()
     time.sleep(1)
 
-    # Choix "Première consultation enfant"
     motif = driver.find_element(By.XPATH, '//span[contains(text(), "Première consultation enfant")]')
     motif.click()
     time.sleep(3)
